882f18f23e5ed07b9ff98422bb8a38dbd3aa34b9
Ash19-88 <ayelenllampa88@gmail.com>
2023-08-27 19:47:10 -0300

    Login actualizado, landing, reset y algunas rutas.

--- client/src/Components/Login.tsx
+++ client/src/Components/Login.tsx
@@ -1,23 +1,42 @@
-interface LoginProps {
-  setUsuario: (usuario: boolean) => void;
-}
+import { NavLink, useNavigate } from "react-router-dom";
 
-const Login: React.FC<LoginProps> = ({ setUsuario }) => {
+const Login: React.FC = () => {
+  const navigate = useNavigate();
+
   const handleClick = () => {
-    setUsuario(true);
+    navigate("/main");
   };
   return (
-    <form className="flex flex-col items-center gap-3">
-      <input type="text" placeholder="Email" />
-      <input type="password" placeholder="Password" />
-      <button
-        onClick={handleClick}
-        value="Login"
-        className="bg-blue-500 text-white p-2 rounded-md"
-      >
-        Login
-      </button>
-    </form>
+    <div className="flex justify-start items-start flex-col">
+      <h1 className="text-2xl my-10 font-bold">Login</h1>
+      <form className="flex flex-col items-start gap-3">
+        <span className="font-semibold ml-1 mt-4">Correo electrónico</span>
+        <input
+          type="text"
+          placeholder="user123@thinderpet.com"
+          className="bg-[#D9D9D9] px-4 py-2 rounded-lg"
+        />
+        <span className="font-semibold ml-1 mt-4">Contraseña</span>
+        <input
+          type="password"
+          placeholder="•••••••••"
+          className="bg-[#D9D9D9] px-4 py-2 rounded-lg"
+        />
+        <NavLink to={"/reset"} className="text-[#D9D9D9] text-sm">
+          ¿olvidaste tu contraseña?
+        </NavLink>
+        <button
+          onClick={handleClick}
+          value="Login"
+          className="bg-blue-500 text-white px-4 py-2 rounded-xl"
+        >
+          Ingresar
+        </button>
+        <NavLink to={"/create"} className="mt-12">
+          Si no tienes cuenta, registrate aquí
+        </NavLink>
+      </form>
+    </div>
   );
 };
 
