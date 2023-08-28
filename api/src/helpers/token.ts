60d370635154856cec4969e4184dc4ff129da45b
Emmanuel Medina <57371753+EmmanuelML78@users.noreply.github.com>
2023-08-28 13:44:12 -0500

    solucionado token

--- api/src/helpers/token.ts
+++ api/src/helpers/token.ts
@@ -22,7 +22,7 @@ export const auth = async (req: Request, res: Response, next: NextFunction) => {
 
     const decodedToken: any = jwt.verify(token, SECRET_TOKEN);
 
-    const user = await User.findOne({ where: { id: decodedToken.userId } });
+    const user = await User.findOne({ _id: decodedToken.id });
 
     if (!user) {
       return res.status(401).json({ message: "Unauthorized. User not found" });
