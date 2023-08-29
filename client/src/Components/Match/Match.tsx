8b7c294475d3240e3a38f246f3a17f302a766f70
Rafaric <rafaelstrongoli@gmail.com>
2023-08-29 20:03:09 -0300

    mejorado el match, instalado react-icons

--- client/src/Components/Match/Match.tsx
+++ client/src/Components/Match/Match.tsx
@@ -1,4 +1,7 @@
 import Confetti from "react-confetti";
+import { AiFillHeart } from "react-icons/ai";
+import Avatar from "../Avatar/Avatar";
+
 interface MatchProps {
   handleMatch: () => void;
 }
@@ -11,24 +14,35 @@ const Match: React.FC<MatchProps> = ({ handleMatch }) => {
         height={1080}
         className="bg-grey-800/70 backdrop-blur-sm"
       />
-      <div className="p-10 rounded-2xl w-[736px] h-[500px] absolute top-96 left-1/2 transform -translate-x-1/2 -translate-y-1/2 bg-gray-300 z-50 m-auto">
+      <div className="p-10 rounded-2xl w-[736px] h-[500px] absolute top-96 left-1/2 transform -translate-x-1/2 -translate-y-1/2 bg-gray-300/40 z-50 m-auto backdrop-blur-xl">
         <div
           className="absolute top-3 right-6 cursor-pointer"
           onClick={handleMatch}
         >
           X
         </div>
-        <div className="px-10 mb-8 flex place-content-around">
-          <img src={"perrito1.jpg"} className="w-48 rounded-full" />
-          <img src={"perrito2.jpg"} className="w-48 rounded-full" />
-        </div>
         <h1 className="uppercase font-bold">Pet Match</h1>
+        <div className="px-10 mb-8 flex place-content-around">
+          <div className="flex flex-col gap-4">
+            <Avatar size="large" src="perrito1.jpg" />
+            <h1 className="font-bold bg-slate-300 rounded-md">SHABI</h1>
+          </div>
+          <AiFillHeart size={150} color="red" className="m-auto heartbeat" />
+          <div className="flex flex-col gap-4">
+            <Avatar size="large" src="perrito2.jpg" />
+            <h1 className="font-bold bg-slate-300 rounded-md">GINA</h1>
+          </div>
+        </div>
         <p className="font-bold my-8">
           Ponte en contacto con el dueño de la mascota para concretar su cita{" "}
         </p>
-        <a href="https://web.whatsapp.com/" target="_blank">
-          <img src={"Contact.png"} className="m-auto hover:cursor-pointer" />
-        </a>
+        <button
+          onClick={() => window.open("https://web.whatsapp.com/", "_blank")}
+          className="border border-black px-8 py-1 flex gap-6 justify-around m-auto items-center rounded-lg hover:border-2 hover:border-neutral-300 hover:shadow-md"
+        >
+          Contáctate
+          <img src={"Contact.png"} width={35} />
+        </button>
       </div>
     </section>
   );
