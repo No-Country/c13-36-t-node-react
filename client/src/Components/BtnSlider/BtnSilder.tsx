fe00111f2796253fb41ed820e5344b1b8af0b21e
De1t4 <mauriciochambicaceres@gmail.com>
2023-08-30 15:41:43 -0300

    responsive

--- client/src/Components/BtnSlider/BtnSilder.tsx
+++ client/src/Components/BtnSlider/BtnSilder.tsx
@@ -1,5 +1,5 @@
 import { useState } from "react";
-import Match from "../Match/Match";
+import Confetti from "react-confetti";
 
 interface BtnSilderProps {
   next: () => void;
@@ -13,22 +13,51 @@ export default function BtnSilder({ next }: BtnSilderProps) {
 
   return (
     <>
-      {matchDogs && <Match handleMatch={handleMatch} />}
-      <figcaption className="w-[736px] mx-auto my-8 flex place-content-between items-center max-md:w-[100%] px-4">
+      {matchDogs ? (
+        <section className="">
+          <Confetti style={{width:"100%", height:"100%"}}/>
+          <div className="p-10 rounded-2xl	 w-96 h-2/4 absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 bg-gray-300 z-50 m-auto h-auto">
+            <div
+              className="absolute top-3 right-6 cursor-pointer"
+              onClick={handleMatch}
+            >
+              <i className="fa-solid fa-xmark text-xl"></i>
+            </div>
+            <div className="px-10 mb-8 flex place-content-around">
+              <img src={"perrito1.jpg"} className="w-20 rounded-full" />
+              <img src={"perrito2.jpg"} className="w-20 rounded-full" />
+            </div>
+            <h1 className="uppercase font-bold">Pet Match</h1>
+            <p className="font-bold my-8">
+              Ponte en contacto con el dueño de la mascota para concretar su
+              cita{" "}
+            </p>
+            <a href="https://web.whatsapp.com/" target="_blank">
+              <img
+                src={"Contact.png"}
+                className="m-auto hover:cursor-pointer"
+              />
+            </a>
+          </div>
+        </section>
+      ) : (
+        ""
+      )}
+      <figcaption className="w-[736px] mx-auto my-8 px-28 flex place-content-between items-center max-md:w-[100%] px-4">
         <button className="bg-gray-300 shadow-md shadow-[10px 10px 17px -4px rgba(0,0,0,0.57)] hover:opacity-70 hover:shadow-inner active:shadow-md rounded-full	p-5">
-          <img src="nop.png" width={60}></img>
+          <img src="nop.png" className="w-14 max-md:w-10"></img>
         </button>
         <button
           className="bg-gray-300 shadow-md shadow-[10px 10px 17px -4px rgba(0,0,0,0.57)] hover:opacity-70 hover:shadow-inner active:shadow-md rounded-full	p-1"
           onClick={handleMatch}
         >
-          <img src="like-nobg.png" width={120}></img>
+          <img src="like-nobg.png" className="w-28 max-md:w-24"></img>
         </button>
         <button
           className="bg-gray-300 shadow-md shadow-[10px 10px 17px -4px rgba(0,0,0,0.57)] hover:opacity-70 hover:shadow-inner active:shadow-md rounded-full hover:cursor-pointer"
           onClick={next}
         >
-          <img src="arrow-next.png" width={110}></img>
+          <img src="arrow-next.png" className="w-24 max-md:w-20"></img>
         </button>
       </figcaption>
     </>
--- client/src/Components/Forms/PetForm.tsx
+++ client/src/Components/Forms/PetForm.tsx
@@ -40,13 +40,13 @@ const PetForm = () => {
           </div>
           <div className="grid grid-cols-2 gap-10 mt-16 max-md: grid grid-rows-2 grid-flow-col gap-10 mt-4">
             <img className="w-48 h-48 max-md:w-full " src="perrito1.jpg" />
-            <img className="w-48 h-48 max-md:w-full" src="perrito2.jpg" />
+            <img className="w-48 h-48 max-md:w-full " src="perrito2.jpg" />
             <img className="w-48 h-48 max-md:w-full" src="perrito3.jpg" />
             <button className="bg-[#D9D9D9] text-white font-extrabold text-9xl transition-transform relative overflow-hidden max-md:w-full">+
             </button>
           </div>
         </div>
-        <button className="w-40 border-2 bg-gray-300 font-bold p-2 rounded-lg mt-4">Guardar Cambios</button>
+        <button className="w-40 border-2 bg-gray-300 font-bold p-2 rounded-lg mt-10">Guardar Cambios</button>
       </form>
     </div>
   );
