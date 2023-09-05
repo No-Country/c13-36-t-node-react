4dc5e841e33ecb39ab80ce47ecb3feb3794a21ab
Ash19-88 <ayelenllampa88@gmail.com>
2023-09-05 01:07:09 -0300

    media query para movil, modificado login

--- client/src/Components/Footer/Footer.tsx
+++ client/src/Components/Footer/Footer.tsx
@@ -4,13 +4,13 @@ export default function Footer() {
   const opciones = ["Nosotros", "Politicas", "Contacto", "Ayuda"];
 
   return (
-    <footer className="bg-[#99A3B0] w-full p-10 flex flex-row justify-around align-baseline ">
+    <footer className="bg-[#99A3B0] w-full mobile:w-[100vw] mobile:flex mobile:flex-col mobile:justify-center mobile:items-center mobile:text-center mobile:align-middle p-10 flex flex-row justify-around align-baseline ">
       <img
         src={"logoFooter.png"}
         alt="footer-logo-landing-page"
         className="w-20"
       />
-      <ul className="flex gap-x-8 my-4 text-[#fff] font-bold decoration-gray-100 max-sm:flex-col max-sm:leading-10">
+      <ul className="flex gap-x-8 my-4 text-[#fff] font-bold decoration-gray-100 max-sm:flex-col max-sm:leading-10 mobile:flex mobile:flex-col">
         {opciones.map((opcion) => {
           return <li className="cursor-pointer">{opcion}</li>;
         })}
