d0110aab8529336e4c379baeeff2f9996f6a761f
Ash19-88 <ayelenllampa88@gmail.com>
2023-09-09 17:48:42 -0300

    traducciones en menu

--- client/src/i18n.ts
+++ client/src/i18n.ts
@@ -9,6 +9,8 @@ import footer_es from "./locales/footer/footer_es.json";
 import footer_en from "./locales/footer/footer_en.json";
 import login_es from "./locales/login/login_es.json";
 import login_en from "./locales/login/login_en.json";
+import menu_es from "./locales/menu/menu_es.json";
+import menu_en from "./locales/menu/menu_en.json";
 
 console.log(i18n.language);
 i18n
@@ -26,12 +28,14 @@ i18n
         landing: landing_es,
         footer: footer_es,
         login: login_es,
+        menu: menu_es,
       },
       en: {
         navbar: en,
         landing: landing_en,
         footer: footer_en,
         login: login_en,
+        menu: menu_en,
       },
     },
   });
